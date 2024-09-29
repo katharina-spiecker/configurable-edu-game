da9516c0f421ecf37bb46d717182fc34e887c309
katharina-spiecker <kathispiecker@gmail.com>
2024-09-29 17:07:28 +0200

    add loading screen

--- scenes/gamestart.js
+++ scenes/gamestart.js
@@ -6,33 +6,61 @@ export default class GameStart extends Phaser.Scene {
     super("GameStart");
   }
 
+  preload() {
+    this.load.spritesheet('spriteSheet', '../assets/kenney_pixel-platformer/Tilemap/tilemap-characters_packed.png', {
+      frameWidth: 24,
+      frameHeight: 24
+    });
+  }
+
   create() {
-    // fetch("http://localhost:3000/api/topics/f43760a3-55c3-4d6c-a40e-98e606136ac7")
-    // .then(res => {
-    //   if (!res.ok) {
-    //     throw new Error(`Failed to fetch quiz: Error with status ${res.status}`)
-    //   }
-    //   return res.json();
-    // })
-    // .then(data => {
-    //   console.log(data)
-    //   this.registry.set("topicName", data.topicName)
-    //   this.registry.set("quiz", data.quiz)
-    // })
-    // .catch(err => console.error(err))
 
-    const textBtn = this.add.text(sizes.width / 2, sizes.height / 2, "Game start", {
-      fontSize: "25px Arial",
-      color: "#fff",
-      backgroundColor: '#007bff',
-      padding: { x: 20, y: 10 },
-      align: 'center'
-    })
-    .setOrigin(0.5, 0.5)
-    .setInteractive()
+    // Füge Text Loading
+    const loadingText = this.add.text(sizes.width / 2, sizes.height / 2 + 100, "Loading...", {font: "30px Arial", fill: "#ffffff"}).setOrigin(0.5, 0.5)
 
-    textBtn.on('pointerdown', () => {
-      this.scene.start("MainGame")
+    // Zeige Animation of Spielcharacter während des Ladens
+    this.anims.create({
+      key: 'loading_animation',
+      frames: this.anims.generateFrameNumbers('spriteSheet', { start: 4, end: 5 }),
+      frameRate: 3,
+      repeat: -1
+    });
+    const loadingImage = this.add.sprite(sizes.width / 2, sizes.height / 2, 'spriteSheet', 4);
+    loadingImage.setScale(5);
+    loadingImage.play('loading_animation');
+
+
+    // load game data
+    // f43760a3-55c3-4d6c-a40e-98e606136ac7 cf3ab749-789f-405d-9741-bf6770ac8bed
+    const testQuizId = "cf3ab749-789f-405d-9741-bf6770ac8bed"
+    fetch(`http://localhost:3000/api/topics/${testQuizId}`)
+    .then(res => {
+      if (!res.ok) {
+        throw new Error(`Failed to fetch quiz: Error with status ${res.status}`)
+      }
+      return res.json();
     })
+    .then(data => {
+      loadingImage.destroy();
+      loadingText.destroy();
+      // speichere Quizdaten, damit verfügbar in anderen Szenen
+      this.registry.set("topicName", data.topicName)
+      this.registry.set("quiz", data.quiz)
+      // Button zum Spielbeginn hinzufügen
+      const textBtn = this.add.text(sizes.width / 2, sizes.height / 2, "Start", {
+        fontSize: "25px Arial",
+        color: "#fff",
+        backgroundColor: '#007bff',
+        padding: { x: 20, y: 10 },
+        align: 'center'
+      })
+      .setOrigin(0.5, 0.5)
+      .setInteractive()
+      // Event Listener: bei Klick auf Button startet Spiel
+      textBtn.on('pointerdown', () => {
+        this.scene.start("MainGame")
+      })
+    })
+    .catch(err => console.error(err))
   }
 }
